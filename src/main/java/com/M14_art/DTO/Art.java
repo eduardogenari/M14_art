2efe2cde2632485201f7f4084f13c4a3ac41992b
Emgenari <emgenari@gmail.com>
2021-04-02 18:32:55 +0200

    02-04-c

--- src/main/java/com/M14_art/DTO/Art.java
+++ src/main/java/com/M14_art/DTO/Art.java
@@ -1,5 +1,63 @@
 package com.M14_art.DTO;
 
-public class Art {
+import java.io.Serializable;
+import java.util.Calendar;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="arts")
+public class Art implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String author="unknown";
+	private final Calendar date = Calendar.getInstance();
+	
+	@ManyToOne
+	public Shop shop;
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public Shop getShop() {
+		return shop;
+	}
+	public void setShop(Shop shop) {
+		this.shop = shop;
+	}
+	public Calendar getDate() {
+		return date;
+	}
+	
+	public Art(Long id, String title, String author, Shop shop) {
+		super();
+		this.id = id;
+		this.title = title;
+		setAuthor(author);
+		this.shop = shop;
+	}
+	
+	public Art() {
+		super();
+	}	
+	
+	private static final long serialVersionUID = 1L;
 }
--- src/main/java/com/M14_art/DTO/Shop.java
+++ src/main/java/com/M14_art/DTO/Shop.java
@@ -1,5 +1,60 @@
 package com.M14_art.DTO;
 
-public class Shop {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="shops")
+public class Shop implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Integer capacity;
+	
+	@OneToMany(mappedBy="shop")
+	public List<Art> art;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(Integer capacity) {
+		this.capacity = capacity;
+	}
+	public List<Art> getArt() {
+		return art;
+	}
+	public void setArt(List<Art> art) {
+		this.art = art;
+	}
+
+	public Shop(Long id, String name, Integer capacity, List<Art> art) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.capacity = capacity;
+		this.art = art;
+	}
+
+	public Shop() {
+		super();
+	}
+
+	private static final long serialVersionUID = 1L;
 
 }
